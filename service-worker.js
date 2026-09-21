9d266bf87d5cc8cf143b44ec8eb1bf3f883e671a
valecicc <vale.cicc@gmail.com>
2026-09-21 13:41:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'max-gm-v1.6.8';
+const CACHE = 'max-gm-v1.6.9';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
