3abeeb4097aaa6c8307c5eb4d1564dc8217cc556
valecicc <vale.cicc@gmail.com>
2026-09-21 08:53:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,15 @@
-const CACHE = 'max-gm-v1.6.4';
+const CACHE = 'max-gm-v1.6.5';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(CORE))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE && k.startsWith('max-gm-')).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE && k.startsWith('max-gm-')).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   const req = event.request;
@@ -23,20 +17,12 @@ self.addEventListener('fetch', event => {
   const url = new URL(req.url);
   if (url.origin !== self.location.origin) return;
   if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req, {cache:'no-store'}).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put('./index.html', copy));
-        return res;
-      }).catch(() => caches.match('./index.html'))
-    );
+    event.respondWith(fetch(req, {cache:'no-store'}).then(res => {
+      const copy = res.clone(); caches.open(CACHE).then(c => c.put('./index.html', copy)); return res;
+    }).catch(() => caches.match('./index.html')));
     return;
   }
-  event.respondWith(
-    fetch(req, {cache:'no-store'}).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(req, copy));
-      return res;
-    }).catch(() => caches.match(req))
-  );
+  event.respondWith(fetch(req, {cache:'no-store'}).then(res => {
+    const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return res;
+  }).catch(() => caches.match(req)));
 });
